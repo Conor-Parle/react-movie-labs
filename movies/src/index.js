597e17d8943c7d592e3d703bf80904b661315ca2
Conor Parle <conorparle@Conors-Air.wit.ie>
2024-03-19 09:25:16 +0000

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,14 @@
+import MovieDetailsPage from './pages/movieDetailsPage'
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const sample = {
   adult: false,
@@ -87,8 +95,8 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
