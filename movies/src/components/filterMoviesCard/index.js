c683a2f62009a2b5c38cb60b17994a678367b88c
Conor Parle <conorparle@Conors-Air.wit.ie>
2024-03-19 10:35:44 +0000

    Added filtering of movie list.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -40,8 +40,8 @@ export default function FilterMoviesCard(props) {
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
-    // Completed later
-  };
+    props.onUserInput(type, value)   // NEW
+  }
   const handleTextChange = e => {
     handleChange(e, "name", e.target.value)
   }
--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -7,6 +7,24 @@ import Grid from "@mui/material/Grid";
 const MovieListPage = (props) => {
   const [movies, setMovies] = useState([]);
 
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -28,10 +46,13 @@ const MovieListPage = (props) => {
       </Grid>
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <FilterCard />
+        <FilterCard
+      onUserInput={handleChange}
+      titleFilter={nameFilter}
+      genreFilter={genreFilter}
+    />
         </Grid>
-        <MovieList movies={movies}></MovieList>
-      </Grid>
+        <MovieList movies={displayedMovies} />      </Grid>
     </Grid>
   );
 };
