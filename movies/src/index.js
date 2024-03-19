e119d7adf0694e4bf872f8ab303e4762379edb4d
Conor Parle <conorparle@Conors-Air.wit.ie>
2024-03-19 09:21:01 +0000

    Movie Details page components.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HomePage from "./pages/homePage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const sample = {
+  adult: false,
+  backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
+  belongs_to_collection: {
+    id: 10,
+    name: "Star Wars Collection",
+    poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
+    backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg",
+  },
+  budget: 200000000,
+  genres: [
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 28,
+      name: "Action",
+    },
+  ],
+  homepage:
+    "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+  id: 181808,
+  imdb_id: "tt2527336",
+  original_language: "en",
+  original_title: "Star Wars: The Last Jedi",
+  overview:
+    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+  popularity: 44.208,
+  poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  production_companies: [
+    {
+      id: 1,
+      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
+      name: "Lucasfilm",
+      origin_country: "US",
+    },
+    {
+      id: 11092,
+      logo_path: null,
+      name: "Ram Bergman Productions",
+      origin_country: "US",
+    },
+    {
+      id: 2,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "Walt Disney Pictures",
+      origin_country: "US",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "US",
+      name: "United States of America",
+    },
+  ],
+  release_date: "2017-12-13",
+  revenue: 1332459537,
+  runtime: 152,
+  spoken_languages: [
+    {
+      iso_639_1: "en",
+      name: "English",
+    },
+  ],
+  status: "Released",
+  tagline: "Darkness rises... and light to meet it",
+  title: "Star Wars: The Last Jedi",
+  video: false,
+  vote_average: 7,
+  vote_count: 9692,
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const App = () => {
+  return (
+      <HomePage movies={movies} />
+  );
+};
+
+const rootElement = createRoot( document.getElementById("root") )
+rootElement.render(<App />);
