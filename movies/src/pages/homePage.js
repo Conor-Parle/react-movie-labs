bb91ff77af7cbd2248e57b203a4c60c0ec10adbb
Conor Parle <conorparle@Conors-Air.wit.ie>
2024-03-19 10:50:58 +0000

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from "react"; 
-import Header from "../components/headerMovieList";
-import FilterCard from "../components/filterMoviesCard";
-import MovieList from "../components/movieList";
-import Grid from "@mui/material/Grid";
+import React, { useState, useEffect } from "react";
+import { getMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage'
 
-const MovieListPage = (props) => {
+
+const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
-
-  const [nameFilter, setNameFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
-
-  const genreId = Number(genreFilter);
-
-  let displayedMovies = movies
-    .filter((m) => {
-      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-    })
-    .filter((m) => {
-      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
-    });
-
-  const handleChange = (type, value) => {
-    if (type === "name") setNameFilter(value);
-    else setGenreFilter(value);
-  };
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
 
   const addToFavorites = (movieId) => {
     const updatedMovies = movies.map((m) =>
@@ -33,34 +16,17 @@ const MovieListPage = (props) => {
   };
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json);
-        return json.results;
-      })
-      .then((movies) => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
 
   return (
-    <Grid container sx={{ padding: "20px" }}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-        <FilterCard
-      onUserInput={handleChange}
-      titleFilter={nameFilter}
-      genreFilter={genreFilter}
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      selectFavorite={addToFavorites}
     />
-        </Grid>
-        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />      </Grid>
-    </Grid>
   );
 };
-export default MovieListPage;
+export default HomePage;
--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -5,6 +5,7 @@ import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { useParams } from 'react-router-dom';
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
   const { id } = useParams();
@@ -12,29 +13,16 @@ const MoviePage = (props) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        // console.log(movie)
-        setMovie(movie);
-      });
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        // console.log(images)
-        setImages(images);
-      });
-      // eslint-disable-next-line
+    getMovieImages(id).then((images) => {
+      setImages(images);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
