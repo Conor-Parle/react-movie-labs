931ea67524a37b283d4b803007c227d4af681889
Conor Parle <conorparle@conors-air.home>
2024-03-30 16:27:30 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcoming.js
+++ movies/src/pages/upcoming.js
@@ -2,13 +2,10 @@ import React from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import { getUpcomingMovies } from "../api/tmdb-api"; 
-import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const UpcomingMoviesPage = () => {
   const { data: upcomingMovies = [], isLoading, isError } = useQuery('upcomingMovies', getUpcomingMovies);
-
-  const addToFavorites = (movieId) => {
-  };
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching data</div>;
@@ -17,7 +14,7 @@ const UpcomingMoviesPage = () => {
     <PageTemplate
       title="Upcoming Movies"
       movies={upcomingMovies}
-      action={(movie) => <AddToFavoritesIcon movie={movie} />} // Specify action prop to render AddToFavoritesIcon
+      action={(movie) => <PlaylistAddIcon />}
     />
   );
 };
