5eabfdfe568b3ee8406f03e90212a3164a989466
Conor Parle <conorparle@conors-air.home>
2024-03-31 20:11:44 +0100

    Added import to try get movie recommendations

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from '../api/tmdb-api'
+import { getMovie, getMovieRecommendations } from '../api/tmdb-api';
 import { useQuery } from "react-query";
 import Spinner from '../components/spinner'
 
