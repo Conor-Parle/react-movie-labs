c0b19e5669f85fbc5a388ffafa9d36e3eaac264d
Conor Parle <conorparle@conors-air.home>
2024-03-31 16:22:00 +0100

    updated api to include NowPlayingMovies

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -98,4 +98,19 @@ export const getMovie = (args) => {
       });
   };
   
+  export const getNowPlayingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .then((json) => json.results)
+    .catch((error) => {
+      throw error;
+    });
+  };
   
